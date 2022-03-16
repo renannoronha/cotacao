98a20e1229f6575249b407e8278e519d783caf35
Renan Noronha <renancnoronha@gmail.com>
2022-03-16 16:21:01 -0300

    home page

--- base/views.py
+++ base/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from django.views.generic import View
 
 # Create your views here.
-class HomeView(View):
-    template_name = 'base/base.html'
+class BaseView(View):
+    def get_context_data(self, **kwargs):
+        context = {
+            'version': 1,
+        }
+        return context
+
+class HomeView(BaseView):
+    template_name = 'base/home.html'
+    title = 'Página Inicial'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {})
+        context = self.get_context_data()
+        context['title'] = 'Cotações'
+        return render(request, self.template_name, context)
