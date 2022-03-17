b2b6b66c39ff66130b0640caae15a43cb75968e9
Renan Noronha <renancnoronha@gmail.com>
2022-03-17 15:54:36 -0300

    lista de moedas

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from moeda.models import Moeda
+
 # Create your views here.
 class BaseView(View):
     def get_context_data(self, **kwargs):
@@ -16,4 +18,5 @@ class HomeView(BaseView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data()
         context['title'] = 'Cotações'
+        context['moedas'] = Moeda.objects.all()
         return render(request, self.template_name, context)
