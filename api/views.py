6d62d5980c475040e8bc5d8aeeee1f1b9524fd0c
Renan Noronha <renancnoronha@gmail.com>
2022-03-17 15:52:52 -0300

    cotacoes para o grafico

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.db.models import F
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -6,7 +7,7 @@ from moeda.models import Moeda, Cotacao
 
 from .serializers import MoedaSerializer, CotacaoSerializer
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Create your views here.
 class MoedaList(APIView):
@@ -40,9 +41,9 @@ class CotacaoList(APIView):
     def get(self, request, base, moeda, format=None):
         cotacoes = Cotacao.objects.filter(base=base, moeda=moeda).order_by('-data')
         if request.GET.get('dataInicial', None):
-            cotacoes.filter(data__gte=request.get('dataInicial'))
+            cotacoes = cotacoes.filter(data__gte=request.GET.get('dataInicial'))
         if request.GET.get('dataFinal', None):
-            cotacoes.filter(data__lte=request.get('dataFinal'))
+            cotacoes = cotacoes.filter(data__lte=request.GET.get('dataFinal'))
         
         serializer = CotacaoSerializer(cotacoes, many=True)
         return Response(serializer.data)
@@ -52,11 +53,14 @@ class CotacaoHighchartList(APIView):
     Cotações das moedas por data para o gráfico.
     """
     def get(self, request, base, moeda, format=None):
-        cotacoes = Cotacao.objects.filter(base=base, moeda=moeda).order_by('-data')
+        cotacoes = Cotacao.objects.all().order_by('-data')
         if request.GET.get('dataInicial', None):
-            cotacoes.filter(data__gte=request.get('dataInicial'))
+            cotacoes = cotacoes.filter(data__gte=request.GET.get('dataInicial'))
         if request.GET.get('dataFinal', None):
-            cotacoes.filter(data__lte=request.get('dataFinal'))
+            cotacoes = cotacoes.filter(data__lte=request.GET.get('dataFinal'))
         
-        data = [[datetime(year=c[0].year, month=c[0].month, day=c[0].day).timestamp()*1000, c[1]] for c in cotacoes.values_list('data', 'cotacao')]
+        base = cotacoes.filter(moeda=base)
+        moeda = cotacoes.filter(moeda=moeda)
+        dolar = cotacoes.filter(moeda__codigo='USD')
+        data = [[datetime(year=b.data.year, month=b.data.month, day=b.data.day).timestamp()*1000, (d.cotacao/b.cotacao)*m.cotacao] for (b, m, d) in zip(base, moeda, dolar)]
         return Response(data)
