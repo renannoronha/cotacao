f8366c274d7423f63a78fe37527ffc51897d26df
Renan Noronha <renancnoronha@gmail.com>
2022-03-17 07:46:00 -0300

    moeda page

--- moeda/views.py
+++ moeda/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from base.views import BaseView
 
 # Create your views here.
+class HomeView(BaseView):
+    template_name = 'moeda/moeda.html'
+    title = 'Moeda'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        context['title'] = 'Cotação'
+        return render(request, self.template_name, context)
