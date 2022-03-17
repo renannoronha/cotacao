814ff148a8ee68d6e8b451f5ab5fe7e3b995b79a
Renan Noronha <renancnoronha@gmail.com>
2022-03-17 09:45:52 -0300

    get cotacoes admin button

--- moeda/admin.py
+++ moeda/admin.py
@@ -3,6 +3,9 @@ from django.contrib import messages
 from django_object_actions import DjangoObjectActions
 
 from .models import *
+from .get_cotacoes import get_cotacoes
+
+from datetime import datetime
 
 # Register your models here.
 @admin.register(Moeda)
@@ -17,12 +20,12 @@ class CotacaoAdmin(DjangoObjectActions, admin.ModelAdmin):
     
     def importar_cotacoes(modeladmin, request, queryset):
         try:
-            # getNewVideos()
+            get_cotacoes(datetime.strptime(str(Cotacao.objects.all().order_by('-data').first().data), '%Y-%m-%d'), datetime.today())
             messages.success(request, "Cotacões atualizadas com sucesso!")
         except Exception as e:
-            messages.error(request, "Erro ao buscar Cotações." + str(e))
+            messages.error(request, "Erro ao buscar Cotações. " + str(e))
 
-    importar_cotacoes.label = "Impotar Cotações"
+    importar_cotacoes.label = "Impotar Últimas Cotações"
     importar_cotacoes.short_description = "Importar cotações a partir da última data registrada no sistema."
 
     changelist_actions = ('importar_cotacoes', )
