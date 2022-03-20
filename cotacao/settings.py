d11986f87c9475b6d900052407e257cab543889b
Renan Noronha <renancnoronha@gmail.com>
2022-03-20 16:16:30 -0300

    django-heroku

--- cotacao/settings.py
+++ cotacao/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config, Csv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,5 @@ CRONJOBS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
