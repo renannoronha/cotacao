0f42e6db4aea9bce32b58af0dda1554e6c3831d6
Renan Noronha <renancnoronha@gmail.com>
2022-03-20 13:26:38 -0300

    adicionei nome as urls

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import MoedaList, MoedaDetail, CotacaoList, CotacaoHighchartList
 
 urlpatterns = [
-    path('moedas/', MoedaList.as_view()),
-    path('moedas/<str:pk>/', MoedaDetail.as_view()),
-    path('cotacoes/<str:base>/<str:moeda>/', CotacaoList.as_view()),
-    path('grafico/<str:base>/<str:moeda>/', CotacaoHighchartList.as_view()),
+    path('moedas/', MoedaList.as_view(), name='list_moedas'),
+    path('moedas/<str:pk>/', MoedaDetail.as_view(), name='detail_moedas'),
+    path('cotacoes/<str:base>/<str:moeda>/', CotacaoList.as_view(), name='list_cotacao'),
+    path('grafico/<str:base>/<str:moeda>/', CotacaoHighchartList.as_view(), name='list_highchart'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
