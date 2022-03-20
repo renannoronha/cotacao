fe68c272ca731bc49717228e3a302cf0c29fef6a
Renan Noronha <renancnoronha@gmail.com>
2022-03-20 16:19:35 -0300

    erro schema heroku

--- cotacao/settings.py
+++ cotacao/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config, Csv
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,4 +146,6 @@ CRONJOBS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
