3516aadc8eea183ad85406fd60a4123c5f76daad
Renan Noronha <renancnoronha@gmail.com>
2022-03-20 13:26:47 -0300

    teste de urls

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .views import MoedaList, MoedaDetail, CotacaoList, CotacaoHighchartList
 
 # Create your tests here.
+class TestUrls(TestCase):
+
+    def test_list_moedas_url_resolves(self):
+        self.assertEquals(resolve(reverse('list_moedas')).func.view_class, MoedaList)
+
+    def test_detail_moedas_url_resolves(self):
+        self.assertEquals(resolve(reverse('detail_moedas', args=['some-arg'])).func.view_class, MoedaDetail)
+
+    def test_list_cotacao_url_resolves(self):
+        self.assertEquals(resolve(reverse('list_cotacao', args=['some-arg1', 'some-arg2'])).func.view_class, CotacaoList)
+
+    def test_list_highchart_url_resolves(self):
+        self.assertEquals(resolve(reverse('list_highchart', args=['some-arg', 'some-arg2'])).func.view_class, CotacaoHighchartList)
--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .views import HomeView
 
 # Create your tests here.
+class TestUrls(TestCase):
+
+    def test_home_url_resolves(self):
+        self.assertEquals(resolve(reverse('home')).func.view_class, HomeView)
